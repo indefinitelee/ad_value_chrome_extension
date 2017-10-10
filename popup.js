dfd26d380504cf037f4b8ec2e5fab35e3e5744eb
lee <leerwilson@gmail.com>
2017-10-10 13:27:22 -0400

    set up history

--- popup.js
+++ popup.js
@@ -10,33 +10,48 @@ var cost;
 var count = 0;
 var storage;
 var url;
+var history;
+var totalCost;
+
+// DOM of popup.html
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.tabs.executeScript(scriptFromFile, getExecutionResult);
+  // why do we need to set storage here?
+  storage = chrome.storage.local;
+  chrome.storage.local.get("count", receiveStorageValue);
+  blocked = document.getElementById("frames-blocked");
+  cost = document.getElementById("cost");
+});
+
+var receiveStorageValue = function(value) {
+  console.log("value from storage:", value);
+  if (!value || Object.keys(value).length === 0) {
+    count = 0;
+  } else {
+    count = value["count"];
+  }
+  counter.innerHTML = "count from storage: " + count;
+};
 
 var getExecutionResult = function(resultArray) {
   // console.log(resultArray, "result array");
   if (resultArray[0] && typeof resultArray[0] === "number") {
     blocked.innerHTML = resultArray[0];
     cost.innerHTML = revPerPage;
-    chrome.storage.local.set(
-      {
-        count: resultArray[0] + count,
-        url: url
-      },
-      () => {}
-    );
-    console.log(chrome.storage.local, "saved to chrome storage");
+    // can this just be storage.set since we assign storage on above?
+    chrome.storage.local.set({
+      count: resultArray[0] + count,
+      url: url
+    });
+    console.log(chrome.storage.local, "saved to chrome storage I hope");
   } else {
     console.log("error");
   }
 };
-// DOM of popup.html or current tab?
-document.addEventListener("DOMContentLoaded", () => {
-  chrome.tabs.executeScript(scriptFromFile, getExecutionResult);
-  // why do we need to set storage here?
-  storage = chrome.storage.local;
-  // console.log(chrome.storage);
-  // console.log("loaded popupDOM");
-  blocked = document.getElementById("frames-blocked");
-  cost = document.getElementById("cost");
+
+// get tab url
+url = chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+  console.log("the url is", tabs[0].url);
 });
 
 // there's no need for this to be a separate file
@@ -45,8 +60,3 @@ const revPerPage = 0.045; // guesstimation of google revenue per page
 const perAdValue = function(revPerPage, frames) {
   return revPerPage / frames;
 };
-
-// get tab url
-url = chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-  console.log("the url is", tabs[0].url);
-});
